b222224421138257f8faba65aaee7f078b6837d8
ignaciocorrea1 <ig.correar@duocuc.cl>
2024-11-07 23:51:33 -0300

    Update
    - Se agrego el tipo de usuario

--- api/models.py
+++ api/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoUsuario(models.Model):
+    id = models.CharField(primary_key=True, max_length=2, db_column="idTipo")
+    descripcion = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.descripcion
+
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True, db_column="idUsuario")
     rut = models.CharField(max_length=12)
@@ -9,7 +17,8 @@ class Usuario(models.Model):
     materno = models.CharField(max_length=30)
     correo = models.CharField(max_length=30)
     contrasenia = models.CharField(max_length=30)
-
+    tipo = models.ForeignKey("TipoUsuario", on_delete=models.CASCADE, db_column="idTipo")
+    
     def __str__(self):
         return self.rut
     
